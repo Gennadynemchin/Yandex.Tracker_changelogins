7405f092f399bf3eb2b2b0731662cb09917e5fb2
Gennady Nemchin <gennadynemchin@gmail.com>
2024-07-11 22:03:35 +0300

    Add additional lists of unique users

--- mapping_users.py
+++ mapping_users.py
@@ -11,7 +11,6 @@ org_id = os.getenv("ORGID")
 
 def get_users(token, org_id):
     headers = {"X-Cloud-Org-Id": f"{org_id}", "Authorization": f"OAuth {token}"}
-    print(headers)
     response = requests.get("https://api.tracker.yandex.net/v2/users", headers=headers)
     elements = response.json()
     return elements
@@ -44,22 +43,38 @@ def extract_users(elements):
     return cloud_users, directory_users
 
 
-def match_and_write_to_file(cloud_users, directory_lookup, filename="to.txt"):
-    with open(filename, "w") as file:
+def match_and_write_to_file(
+                            cloud_users,
+                            directory_users,
+                            directory_lookup,
+                            cloud_lookup,
+                            filename="to.txt",
+                            cloud_unique_users="unique_cloud_users.txt",
+                            directory_unique_users="unique_directory_users.txt"
+                            ):
+    with (open(filename, "w") as file,
+            open(cloud_unique_users, "w") as file_unique_cloud,
+            open(directory_unique_users, "w") as file_unique_directory):
         for cloud_user in cloud_users:
             email = cloud_user['email']
             if email in directory_lookup:
                 directory_user = directory_lookup[email]
                 print(cloud_user['id'], directory_user['id'], directory_user['email'])
                 file.write(f"{cloud_user['id']} {directory_user['id']} #\n")
+            else:
+                file_unique_cloud.write(f"{cloud_user['id']}\n")
+        for directory_user in directory_users:
+            if directory_user["email"] not in cloud_lookup:
+                file_unique_directory.write(f"{directory_user["id"]}\n")
 
 
 def process_users(elements):
     cloud_users, directory_users = extract_users(elements)
     directory_lookup = {user['email']: user for user in directory_users}
+    cloud_lookup = {user['email']: user for user in cloud_users}
 
-    if not os.path.isfile("test.txt"):
-        match_and_write_to_file(cloud_users, directory_lookup)
+    if not os.path.isfile("to.txt"):
+        match_and_write_to_file(cloud_users, directory_users, directory_lookup, cloud_lookup)
 
 
 if __name__ == "__main__":
